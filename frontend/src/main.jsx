a52b60afefeb72a065085a0e96e19ebab424d004
Kyohei-takiyama <takiyamakyohei@gmail.com>
2023-06-23 16:34:32 +0900

    create/redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import "./index.css";
+import store from "./store.js";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
